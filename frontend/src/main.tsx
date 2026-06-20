47ca21bcbad57a580f3bc4e8f2066b0a6751e4eb
adam <adam.roy724@gmail.com>
2026-06-20 23:27:04 +0900

    Rehaul of css file organization. Divided each section into its own file for more readablity and accessibilty.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-import './style.css'
+import './styles/bossDashboard.css'
+import './styles/customerDashboard.css'
+import './styles/dashboardLayout.css'
+import './styles/hero.css'
+import './styles/languageSwticher.css'
+import './styles/loginPage.css'
+import './styles/modal.css'
+import './styles/utility.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
